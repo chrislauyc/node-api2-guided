e299328c99898e691422c476d3c005ea7a9b7c6c
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-03 19:04:33 +0200

    wip

--- api/dogs/dogs-router.js
+++ api/dogs/dogs-router.js
@@ -1,4 +1,8 @@
 // dogs endpoints will come here
 const express = require('express')
-
+const router = express.Router()
 const Dog = require('./dogs-model')
+
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -11,34 +11,7 @@ server.use('/api/adopters', adoptersRouter); // plug router in server
 
 const Dog = require('./dogs/dogs-model');
 
-// DOGS ENDPOINTS
-// DOGS ENDPOINTS
-// DOGS ENDPOINTS
-// using async/await
-server.get('/api/dogs', async (req, res) => {
-  try {
-    // if something crashes here
-    console.log('getting dogs with async/await!')
-    const dogs = await Dog.find() // looks like sync code!!
-    res.status(200).json(dogs)
-  } catch (err) {
-    // we can "recover" here, take a look at the error (err)
-    res.status(500).json({ message: `Something happened: ${err.message}` })
-  }
-})
 
-server.get('/api/dogs', (req, res) => { // using ES6 promises
-  Dog.find()
-    .then(dogs => {
-      res.status(200).json(dogs);
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the dogs',
-      });
-    });
-});
 
 // OTHER ENDPOINTS
 // OTHER ENDPOINTS
