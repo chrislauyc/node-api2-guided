6ed60c2d60ec3fd87ece44d5b499d47611b737fd
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-03 18:55:14 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const adoptersRouter = require('./adopters/adopters-router');
 const server = express();
 
 server.use(express.json());
+server.use(adoptersRouter);
 
 const Dog = require('./dogs/dogs-model');
 
