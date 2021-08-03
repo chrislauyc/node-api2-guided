29290405bd82c81b4718acb7f6ff615da41bb72f
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-03 18:51:57 +0200

    wip

--- api/adopters/adopters-router.js
+++ api/adopters/adopters-router.js
@@ -7,7 +7,7 @@ const router = express.Router()
 // ADOPTERS ENDPOINTS
 // ADOPTERS ENDPOINTS
 // ADOPTERS ENDPOINTS
-server.get('/api/adopters', (req, res) => {
+router.get('/api/adopters', (req, res) => {
   Adopter.find(req.query)
     .then(adopters => {
       res.status(200).json(adopters);
@@ -20,7 +20,7 @@ server.get('/api/adopters', (req, res) => {
     });
 });
 
-server.get('/api/adopters/:id', (req, res) => {
+router.get('/api/adopters/:id', (req, res) => {
   Adopter.findById(req.params.id)
     .then(adopter => {
       if (adopter) {
@@ -37,7 +37,7 @@ server.get('/api/adopters/:id', (req, res) => {
     });
 });
 
-server.get('/api/adopters/:id/dogs', (req, res) => {
+router.get('/api/adopters/:id/dogs', (req, res) => {
   Adopter.findDogs(req.params.id)
     .then(dogs => {
       if (dogs.length > 0) {
@@ -54,7 +54,7 @@ server.get('/api/adopters/:id/dogs', (req, res) => {
     });
 });
 
-server.post('/api/adopters', (req, res) => {
+router.post('/api/adopters', (req, res) => {
   Adopter.add(req.body)
     .then(adopter => {
       res.status(201).json(adopter);
@@ -67,7 +67,7 @@ server.post('/api/adopters', (req, res) => {
     });
 });
 
-server.delete('/api/adopters/:id', (req, res) => {
+router.delete('/api/adopters/:id', (req, res) => {
   Adopter.remove(req.params.id)
     .then(count => {
       if (count > 0) {
@@ -84,7 +84,7 @@ server.delete('/api/adopters/:id', (req, res) => {
     });
 });
 
-server.put('/api/adopters/:id', (req, res) => {
+router.put('/api/adopters/:id', (req, res) => {
   const changes = req.body;
   Adopter.update(req.params.id, changes)
     .then(adopter => {
