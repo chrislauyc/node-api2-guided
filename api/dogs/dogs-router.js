0be958169f9ef2d4a6d9927cd540b61cf635cc94
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-03 19:05:19 +0200

    wip

--- api/dogs/dogs-router.js
+++ api/dogs/dogs-router.js
@@ -3,6 +3,33 @@ const express = require('express')
 const router = express.Router()
 const Dog = require('./dogs-model')
 
+// DOGS ENDPOINTS
+// DOGS ENDPOINTS
+// DOGS ENDPOINTS
+// using async/await
+router.get('/api/dogs', async (req, res) => {
+  try {
+    // if something crashes here
+    console.log('getting dogs with async/await!')
+    const dogs = await Dog.find() // looks like sync code!!
+    res.status(200).json(dogs)
+  } catch (err) {
+    // we can "recover" here, take a look at the error (err)
+    res.status(500).json({ message: `Something happened: ${err.message}` })
+  }
+})
 
+router.get('/api/dogs', (req, res) => { // using ES6 promises
+  Dog.find()
+    .then(dogs => {
+      res.status(200).json(dogs);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the dogs',
+      });
+    });
+});
 
 module.exports = router
